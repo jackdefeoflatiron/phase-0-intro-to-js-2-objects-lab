e391ac595492741842f27cb7de15137d8bc1b5c4
John DeFeo <jackd3243@gmail.com>
2021-09-30 19:30:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const = employees {
+    object = employee( key1, key2)
+        key1 : name ,
+        key2 : streetAddress,
+}
+function updateEmployeeWithKeyAndValue(employees,name, streetAddress){
+    name("Sam")
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employees,name, streetAddress) {
+    name.splice("Sam");
+    streetAddress.splice("11 Broadway")
+}
+function updateEmployeeWithKeyAndValue(employees,name, streetAddress) {
+    streetAddress.unshift("11 Broadway")
+}
+function deleteFromEmployeeByKey(employees,name, streetAddress) {
+    
+}
+function destructivelyDeleteFromEmployeeByKey(employees,name, streetAddress) {
+
+}
